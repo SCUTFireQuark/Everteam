566fc9525e5115ea170d4e4cc4ce12f2d8686562
CAIJIAXUN <790297338@qq.com>
2014-08-27 11:06:39 +0800

    改了张明龙的BUG

---
+++ PersonalMemo BY ST-3.1.6/PersonalMemo/STCreatePersonalMemoViewController.h
@@ -0,0 +1,13 @@
+//
+//  STCreatePersonalMemoViewController.h
+//  PersonalMemo
+//
+//  Created by st on 14-7-10.
+//  Copyright (c) 2014年 st. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface STCreatePersonalMemoViewController : UIViewController
+@property NSString * theme;
+@end
---
+++ PersonalMemo BY ST-3.1.6/PersonalMemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  PersonalMemo
+//
+//  Created by st on 14-7-10.
+//  Copyright (c) 2014年 st. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "STAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([STAppDelegate class]));
+    }
+}
