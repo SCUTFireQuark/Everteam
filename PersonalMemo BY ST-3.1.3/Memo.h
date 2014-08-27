23ea85b5473f5cedb3c79b681e0456104d9ff4c2
sutong1994 <ST@stdeMacBook-Air.local>
2014-08-27 11:31:05 +0800

    上传了PersonalMemo 3.1.3

---
+++ PersonalMemo BY ST-3.1.3/Memo.h
@@ -0,0 +1,24 @@
+//
+//  Memo.h
+//  PersonalMemo
+//
+//  Created by st on 14-7-10.
+//  Copyright (c) 2014年 st. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+
+
+@interface Memo : NSManagedObject
+
+@property (nonatomic, retain) NSString * summary;
+@property (nonatomic, retain) NSString * details;
+@property (nonatomic, retain) NSDate * remindTime;
+@property (nonatomic, retain) NSString * theme;
+@property (nonatomic, retain) NSNumber * isRemind;
+@property (nonatomic, retain) NSNumber * isHighlight;
+@property (nonatomic, retain) NSString * order;
+@property (nonatomic, retain) NSString * createTime;
+
+@end
